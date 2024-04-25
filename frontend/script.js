eba4b5cb9704c1b61e241e08f6005802740ea786
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-04-25 23:50:23 +0530

    added some js

--- frontend/script.js
+++ frontend/script.js
@@ -23,6 +23,43 @@ function display_Todos(todoArr) {
         todo.classList.add("todo");
         //children
         let todoInfo = document.createElement("div");
+        todoInfo.classList.add("todo-info");
+        let todoBtn = document.createElement("form");
+        todoBtn.classList.add("todo-btn");
+        //Grand Children
+        let todoCompleted = Document.createElement("input");
+        todoCompleted.classList.add("todo-completed");
+        todoCompleted.setAttribute("type", "checkbox");
+        todoCompleted.checked =todoElem.completed;
+        let todoName =document.createElement("p");
+        todoName.classList.add("todo-name");
+        todoName.innerHTML = todoElem.name;
+
+        let todoEdit = document.createElement("button");
+        todoEdit.classList.add("todo-edit");
+        todoEdit.innerHTML = "Edit";
+        todoEdit.addEventListener("click", e => {
+            e.preventDefault() 
+            console.log("open modal");
+
+        }) ;
+        let todoDel =document.createElement("button");
+        todoDel.classList.add("todo-delete");
+        todoDel.innerHTML = "Delete";
+        todoDel.addEventListener("click" , e => {
+            e.preventDefault()
+            console.log("Delete todo");
+        });
+
+        // Append
+        todoInfo.appendChild(todoCompleted);
+        todoInfo.appendChild(todoName);
+        todoBtn.appendChild(todoEdit);
+        todoBtn.appendChild(todoDel);
+        todo.appendChild(todoInfo);
+        todo.appendChild(todoBtn);
+
+        todocontainer.appendChild(todo);
     });
 }
  
