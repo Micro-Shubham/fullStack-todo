e0879adc91dc3eed9714a8b06a33b2c019f95e16
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-04-29 23:30:30 +0530

    render function

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 let btn = document.getElementById("add-todo");
 let title = document.getElementById("input-todo");
+let container = document.getElementById("todo-container").innerHTML;
 
 // array for storing data
 let dataArray = [];
@@ -10,6 +11,18 @@ let value= title.innerHTML;
 addTodo(value);
 });
 
+function addTodo (value) {
+    dataArray.push(value);
+render(dataArray);
+}
 
+function render(todo) {
+    for(let i=0;i<todo.length;i++) {
+    let list = todo[i];
+     let create = document.createElement("div");
+     create.innerHTML = list;
+     container.appendchild(create);
+    }
+}
 
 
