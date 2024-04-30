293fd7e8ac70fdddf995014edf34f07f732a893d
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-04-30 23:48:14 +0530

    js update

--- frontend/script.js
+++ frontend/script.js
@@ -1,28 +1,29 @@
 let btn = document.getElementById("add-todo");
 let title = document.getElementById("input-todo");
 let container = document.getElementById("todo-container");
-
+let id = 1;
 // array for storing data
 let dataArray = [];
 
-//click function 
+//click function
 btn.addEventListener("click", () => {
-let value= title.innerHTML;
-addTodo(value);
+  addTodo(title.value);
 });
 
-function addTodo (value) {
-    dataArray.push(value);
- render(dataArray);
+function addTodo(title) {
+  let object = {
+    Title: title,
+    id: id++,
+  }
+  dataArray.push(object);
+  render(dataArray);
 }
 
 function render(todo) {
-    for(let i=0;i<todo.length;i++) {
+  for (let i = 0; i < todo.length; i++) {
     let list = todo[i];
-     let create = document.createElement("p");
-     create.innerHTML = list;
-     container.appendchild(create);
-    }
+    let create = document.createElement("p");
+    create.innerHTML = list;
+    container.appendchild(create);
+  }
 }
-
-
