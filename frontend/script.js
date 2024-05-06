a9afef12a9b8639de1a1769b4ac6575aa0e4e1a7
Shubham Yadav <shubhamtikkha09@gmail.com>
2024-05-06 22:41:15 +0530

    delete function

--- frontend/script.js
+++ frontend/script.js
@@ -7,29 +7,37 @@ let dataArray = [];
 //click function
 btn.addEventListener("click", () => {
   addTodo(title.value);
-  title.value= "";
+  title.value = "";
 });
 
 function addTodo(title) {
-  let id = Math.floor(Math.random()*10000+1);
+  let id = Math.floor(Math.random() * 10000 + 1);
   let object = {
-    Title : title,
-    Id : id,
+    title: title,
+    id: id,
   };
   dataArray.push(object);
   render(dataArray);
 }
 
 function render(todo) {
-    container.innerHTML ="";
+  container.innerHTML = "";
   for (let i = 0; i < todo.length; i++) {
-    let list = todo[i].Title;
+    let list = todo[i].title;
     let create = document.createElement("p");
     create.innerHTML = list;
-    create.id = todo[i].Id;
+    create.id = todo[i].id;
     container.append(create);
   }
 }
 
-
-
+create.addEventListener("click", () => {
+  for (let i = 0; i < dataArray.length; i++) {
+    if (dataArray[i].id == id) {
+      dataArray.pop();
+    }
+    let arr = [];
+    arr[i] = dataArray[i];
+  }
+  render(arr);
+});
